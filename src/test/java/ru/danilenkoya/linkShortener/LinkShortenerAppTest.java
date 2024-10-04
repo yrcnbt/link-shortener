d57c35069bf5b84f5c872e45381a4c6bb7cb5667
Danilenko Yuriy <yuri.danilenko97@gmail.com>
2024-10-04 15:19:48 +0300

    addTest

---
+++ src/test/java/ru/danilenkoya/linkShortener/LinkShortenerAppTest.java
@@ -0,0 +1,21 @@
+package ru.danilenkoya.linkShortener;
+
+import org.junit.jupiter.api.Test;
+
+public class LinkShortenerAppTest {
+
+    @Test
+    void test() {
+        byte b;
+        short s;
+        int i;
+        long l;
+        float f;
+        double d;
+        boolean bool;
+
+        int[] ints = new int[Integer.MAX_VALUE];
+
+        String str = "abc";
+    }
+}
