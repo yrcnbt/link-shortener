2db09a9cdfe567c0051c50d6844d000725e3a999
Danilenko Yuriy <yuri.danilenko97@gmail.com>
2024-10-04 09:20:31 +0300

    firstCommit

---
+++ src/main/java/ru/danilenkoya/linkShortener/LinkShortenerApp.java
@@ -0,0 +1,12 @@
+package ru.danilenkoya.linkShortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerApp.class, args);
+    }
+}
